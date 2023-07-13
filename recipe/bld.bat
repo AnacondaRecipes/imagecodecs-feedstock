a126d05d3780ce1453464b0498eb0a7b70b5c30c
Lorenzo Pirritano <6698585+lorepirri@users.noreply.github.com>
2023-07-13 14:28:33 +0200

    Apply suggestions from code review
    Co-authored-by: Jean-Christophe Morin <38703886+JeanChristopheMorinPerso@users.noreply.github.com>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,7 @@ rem this old code-base assume there is a libdeflate.lib file to be found.
 rem it was renamed by upstream venture to deflate.lib, which we fix here.
 copy/b %LIBRARY_LIB%\deflate.lib %LIBRARY_LIB%\libdeflate.lib
 
-%PYTHON% -m pip install . --no-deps --no-build-isolation
+%PYTHON% -m pip install . --no-deps --no-build-isolation -v
 
 rem remove created file from PREFIX, so that this copy doesn't end up being
 rem part of the resulting package
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,4 @@
 # Need to add the openjpeg2 cflags
 export CFLAGS="${CFLAGS} $(pkg-config --cflags libopenjp2)"
 
-$PYTHON -m pip install . --no-deps --no-build-isolation
+$PYTHON -m pip install . --no-deps --no-build-isolation -v
