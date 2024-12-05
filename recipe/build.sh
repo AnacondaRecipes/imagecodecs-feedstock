fa9817b7d721564e0526aadc396af6c8fc486fd0
Serhii Kupriienko <skupriienko@anaconda.com>
2024-12-05 15:25:35 +0200

    Increase verbosity

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,4 +3,4 @@
 # Need to add the openjpeg2 cflags
 export CFLAGS="${CFLAGS} $(pkg-config --cflags libopenjp2)"
 
-$PYTHON -m pip install . --no-deps --no-build-isolation -v
+$PYTHON -m pip install . --no-deps --no-build-isolation -vv
