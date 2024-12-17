4c52a7a687d335b0aae24c9174d1b443ccdcd593
skupr-anaconda <79282962+skupr-anaconda@users.noreply.github.com>
2024-12-17 13:58:03 +0200

    imagecodecs 2024.9.22 (#17)
    * 2024.9.22
    
    * Use libaec 1 and libavif 1.1.1 in host
    
    * fix gil
    
    * fix jxrlib
    
    * Fix openjpeg
    
    * Add shebang
    
    * Add whitespaces
    
    * Update libdeflate pinning
    
    * Use lerc on s390x
    
    * Increase verbosity
    
    * Skip more tests on s390x
    
    * Disable libjpeg-turbo
    
    * Delete jpegxl
    
    * Delete zpf, jpegls, brunsli on s390x, and zpf on aarch64
    
    * Add zlib-ng; support py313; skip a test on s390x
    
    * Delete blosc2
    
    * Skip test_cms_identity_transforms on win too
    
    * Pin lcms2 to 1.16
    
    * Skip lcms2 tests
    
    * Fix libtiff pinning
    
    * Remove a patch, update pinnings in host, use numpy >=1.21,<3 in run
    
    * Add test dependencies
    
    * Disable unavailable test deps
    
    * Use tifffile >=2024.12.12 in tests
    
    * Skip tifffile test on s390x
    
    * tifffile for py>=310
    
    * Skip tifffile test on win
    
    * Add a comment about fix-customize-build.patch

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,9 @@ rem this old code-base assume there is a libdeflate.lib file to be found.
 rem it was renamed by upstream venture to deflate.lib, which we fix here.
 copy/b %LIBRARY_LIB%\deflate.lib %LIBRARY_LIB%\libdeflate.lib
 
-%PYTHON% -m pip install . --no-deps --no-build-isolation -v
+set openjpeg=2.3
+
+%PYTHON% -m pip install . --no-deps --no-build-isolation -vv
 
 rem remove created file from PREFIX, so that this copy doesn't end up being
 rem part of the resulting package
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+
 # Need to add the openjpeg2 cflags
 export CFLAGS="${CFLAGS} $(pkg-config --cflags libopenjp2)"
 
-$PYTHON -m pip install . --no-deps --no-build-isolation -v
+$PYTHON -m pip install . --no-deps --no-build-isolation -vv
