d4e0d4dff2c93afb93faa80dea303ee6e059e2f3
Serhii Kupriienko <skupriienko@anaconda.com>
2025-04-04 12:02:20 +0300

    Update staging channels

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,7 @@ rem this old code-base assume there is a libdeflate.lib file to be found.
 rem it was renamed by upstream venture to deflate.lib, which we fix here.
 copy/b %LIBRARY_LIB%\deflate.lib %LIBRARY_LIB%\libdeflate.lib
 
-set openjpeg=2.3
+set openjpeg=2.5
 
 %PYTHON% -m pip install . --no-deps --no-build-isolation -vv
 
